b4f9125898ff7dbe4abf0ea641cb842ab84bb358
khnnyy <152983079+khnnyy@users.noreply.github.com>
2024-07-11 15:46:02 +0800

    Job Order Form | Improvement phase 2

---
+++ src/main/java/com/mycompany/webserverlenin/KeepAliveController.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.webserverlenin;
+
+/**
+ *
+ * @author khanny
+ */
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class KeepAliveController {
+
+    @GetMapping("/health")
+    public String healthCheck() {
+        return "Application is up and running!";
+    }
+}
